d51701b360cefbfe242d7bc8dc16c7861b2146d0
Hans-J. Schmid <knock@myopendoor.de>
2020-05-19 04:47:25 +0200

    uses only sets.

--- powerset.rkt
+++ powerset.rkt
@@ -1,17 +1,16 @@
 #lang racket
 
-; powerset: set -> list of sets
+; powerset: set -> set of sets
 (define (powerset s)
-  (cond [(set-empty? s) (list (set))]
+  (cond [(set-empty? s) (set (set))]
 	[else
-	  (let* ([rst (set-rest s)]
-		 [ps-rst (powerset rst)])
-	    (append (map (lambda (s1)
-			   (set-add s1 (set-first s)))
-			 ps-rst)
-		    ps-rst))]))
+	  (let ([ps-rst (powerset (set-rest s))])
+	    (set-union ps-rst
+		       (list->set (set-map ps-rst
+					   (lambda (s1)
+					     (set-add s1 (set-first s)))))))]))
 
-(length (powerset (set 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20)))
+(set-count (powerset (set 1 2 3 4 5 6 7 8 9 10)))
 (powerset (set 1 2 3))
 (powerset (set 1 2 3 4))
 (powerset (set 1 2 3 4 5))
