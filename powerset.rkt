f53860df65c2dec4758b32aa9a0f820a4a96d8c5
Hans-J. Schmid <knock@myopendoor.de>
2020-05-19 05:29:03 +0200

    uses for/fold from Rosetta Code.
    https://rosettacode.org/wiki/Power_set#Racket

--- powerset.rkt
+++ powerset.rkt
@@ -2,21 +2,15 @@
 
 ; powerset: set -> set of sets
 (define (powerset s)
-  (define (powerset0 s acc)
-    (cond [(set-empty? s) acc]
-	  [else
-	    (powerset0 (set-rest s) 
+  (for/fold ([acc (set(set))])
+	    ([element s])
+	    (set-union acc
 		       (list->set 
-			 (set-union acc
-				    (list->set 
-				      (set-map acc
-					       (lambda (s1) 
-						 (set-add s1 (set-first s))))))))]))
-  (powerset0 s (set (set))))
+			 (set-map acc
+				  (lambda (s1) 
+				    (set-add s1 element)))))))
 
 (set-count (powerset (set 1 2 3 4 5 6 7 8 9 10)))
 (powerset (set 1 2 3))
 (powerset (set 1 2 3 4))
 (powerset (set 1 2 3 4 5))
-
-
