911bdbea2ec700f11850332356285f86fe32d5be
Hans-J. Schmid <knock@myopendoor.de>
2020-05-19 05:25:38 +0200

    uses constructive approach.

--- powerset.rkt
+++ powerset.rkt
@@ -2,16 +2,21 @@
 
 ; powerset: set -> set of sets
 (define (powerset s)
-  (cond [(set-empty? s) (set (set))]
-	[else
-	  (let ([ps-rst (powerset (set-rest s))])
-	    (set-union ps-rst
-		       (list->set (set-map ps-rst
-					   (lambda (s1)
-					     (set-add s1 (set-first s)))))))]))
+  (define (powerset0 s acc)
+    (cond [(set-empty? s) acc]
+	  [else
+	    (powerset0 (set-rest s) 
+		       (list->set 
+			 (set-union acc
+				    (list->set 
+				      (set-map acc
+					       (lambda (s1) 
+						 (set-add s1 (set-first s))))))))]))
+  (powerset0 s (set (set))))
 
 (set-count (powerset (set 1 2 3 4 5 6 7 8 9 10)))
 (powerset (set 1 2 3))
 (powerset (set 1 2 3 4))
 (powerset (set 1 2 3 4 5))
+
 
